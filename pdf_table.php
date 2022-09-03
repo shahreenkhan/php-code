28db69f064e2e05f7fd297cfedea96bff41ece9d
shahreen khan <shareenkhan04@gmail.com>
2022-09-03 12:36:50 +0530

    firstchange

---
+++ pdf_table.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Invoice</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="page" size="A4">
+		<div class="top-section">
+			<div class="address">
+				<div class="address-content">
+					<h2>Company Name</h2>
+					<p></p>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+
+</body>
+</html>
